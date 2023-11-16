84f4b959d69f92acc74b009160d6a4094aae4107
Juergen Furrer <juergen.furrer@gmail.com>
2023-11-16 21:37:44 +0100

    Update version

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@swisscode.sk',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '2.0.1',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '11.5.0-11.99.99',
             'newt' => '3.0.0-3.99.99',
             'dce' => '2.9.0-3.99.99',
         ],
